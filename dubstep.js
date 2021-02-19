ad6d60e1bd0acfcc932626f16935cbda2b348dfc
Lian Araújo Fernandes <liansutil@gmail.com>
2021-02-19 11:36:45 -0300

    remove dub

--- dubstep.js
+++
@@ -1,7 +0,0 @@
-function songDecoder(song){
-  let noWub = song.replace(/WUB/g, ' ')
-  noWub = noWub.split(' ')
-  noWub = noWub.filter(e => e !== '');
-  noWub = noWub.join(" ")
-  return noWub
-}
